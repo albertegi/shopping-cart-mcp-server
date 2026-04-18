e638025e8ac7982926cf23cbfaf9a4f30a17868e
albertegi <albertegi90@gmail.com>
2026-04-18 23:08:17 +0100

    Initial commit

---
+++ src/main/java/com/example/shoppingcartmcp/ShoppingcartmcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartmcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartmcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shoppingcartmcp/ShoppingcartmcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartmcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
